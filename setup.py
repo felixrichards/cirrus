01dc1f4db9b3a0f6f09203b1d62b09206b81dbaf
felixrichards <felixarichards@gmail.com>
2021-08-13 16:33:41 +0100

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="cirrus",
+    version="0.1",
+    author="felixrichards",
+    url="github.com/felixrichards/cirrus",
+    packages=find_packages()
+)
