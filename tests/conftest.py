13e24d012c82a549b43df6646fdca3f98cf689a3
Felix Richards <felixarichards@gmail.com>
2021-10-18 13:48:19 +0100

    Tests for LSBDataset

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,10 @@ import pytest
 
 @pytest.fixture(scope='session')
 def setup_func(request):
-    survey_dir = "D:/Matlas Data/FITS/matlas" 
-    mask_dir = "D:/MATLAS Data/annotations/all3009"
+    survey_dir = "E:/Matlas Data/FITS/matlas"
+    mask_dir = "E:/MATLAS Data/annotations/drawtest3009"
+
+    lsb_survey_dir = "E:/Matlas Data/np"
+    lsb_mask_dir = "E:/MATLAS Data/annotations/consensus"
 
     return locals()
--- tests/test_data.py
+++ tests/test_data.py
@@ -4,15 +4,15 @@ import matplotlib.pyplot as plt
 
 
 def test_combine_classes():
-    mask = np.zeros((20, 5, 5), dtype=int)
+    mask = np.zeros((19, 5, 5), dtype=int)
+    mask[1] = 1
     mask[2] = 1
-    mask[3] = 1
+    mask[4, 2, 1] = 1
     mask[5, 2, 1] = 1
-    mask[6, 2, 1] = 1
-    mask[6, 2, 2] = 1
-    mask[16] = 1
-    mask[17, 1, 2] = 1
-    mask[18] = 1
+    mask[5, 2, 2] = 1
+    mask[15] = 1
+    mask[16, 1, 2] = 1
+    mask[17] = 1
     contaminant_map = CirrusDataset.class_maps['contaminants']['idxs']
     out = combine_classes(mask, contaminant_map)
     
@@ -46,11 +46,57 @@ def test_load(setup_func):
 
 def test_load_classmaps(setup_func):
     dataset = CirrusDataset(setup_func['survey_dir'], setup_func['mask_dir'], class_map='contaminants')
-    item = dataset[0]
-    assert item[1].shape[0] == 3
-    assert torch.all(item[1][0] == 0) and torch.all(item[1][1] == 1) and torch.all(item[1][2] == 0)
-
-    dataset = CirrusDataset(setup_func['survey_dir'], setup_func['mask_dir'], class_map='basic')
     galaxy = 'NGC1121'
     item = dataset.get_galaxy(galaxy)
     assert item[1].shape[0] == 3
+    assert torch.any(item[1][1] == 1) and torch.any(item[1][0] == 1) and torch.any(item[1][2] == 1)
+
+    dataset = CirrusDataset(setup_func['survey_dir'], setup_func['mask_dir'], class_map='basic')
+    item = dataset.get_galaxy(galaxy)
+    assert item[1].shape[0] == 3
+    assert torch.sum(item[1][0] < 100) and torch.any(item[1][1] == 1) and torch.any(item[1][2] == 1)
+
+    dataset = CirrusDataset(setup_func['survey_dir'], setup_func['mask_dir'], class_map='streamstails')
+    item = dataset.get_galaxy(galaxy)
+    assert item[1].shape[0] == 2
+    assert torch.all(item[1][0] == 0) and torch.any(item[1][1] == 1)
+
+    dataset = CirrusDataset(setup_func['survey_dir'], setup_func['mask_dir'], class_map='all')
+    item = dataset.get_galaxy(galaxy)
+    assert item[1].shape[0] == 9
+    assert (
+        torch.any(item[1][0] == 1) and torch.any(item[1][1] == 1) and torch.all(item[1][2] == 0) and
+        torch.any(item[1][3] == 1) and torch.all(item[1][4] == 0) and torch.all(item[1][5] == 0) and
+        torch.any(item[1][6] == 1) and torch.any(item[1][7] == 1) and torch.any(item[1][8] == 1)
+    )
+    
+
+def test_lsb_load_classmaps(setup_func):
+    dataset = LSBDataset(setup_func['lsb_survey_dir'], setup_func['lsb_mask_dir'], bands=['g', 'r'], class_map='contaminants')
+    galaxy = 'NGC1121'
+    item = dataset.get_galaxy(galaxy)
+    dataset.plot_galaxy(galaxy)
+    assert item[1].shape[0] == 3
+    assert torch.any(item[1][1] > 0) and torch.any(item[1][0] > 0) and torch.any(item[1][2] > 0)
+
+    dataset = LSBDataset(setup_func['lsb_survey_dir'], setup_func['lsb_mask_dir'], bands=['g', 'r'], class_map='basic')
+    item = dataset.get_galaxy(galaxy)
+    dataset.plot_galaxy(galaxy)
+    assert item[1].shape[0] == 3
+    assert torch.sum(item[1][0] < 100) and torch.any(item[1][1] > 0) and torch.any(item[1][2] > 0)
+
+    dataset = LSBDataset(setup_func['lsb_survey_dir'], setup_func['lsb_mask_dir'], bands=['g', 'r'], class_map='streamstails')
+    item = dataset.get_galaxy(galaxy)
+    dataset.plot_galaxy(galaxy)
+    assert item[1].shape[0] == 2
+    assert torch.all(item[1][0] == 0) and torch.any(item[1][1] > 0)
+
+    dataset = LSBDataset(setup_func['lsb_survey_dir'], setup_func['lsb_mask_dir'], bands=['g', 'r'], class_map='all')
+    item = dataset.get_galaxy(galaxy)
+    dataset.plot_galaxy(galaxy)
+    assert item[1].shape[0] == 9
+    assert (
+        torch.any(item[1][0] > 0) and torch.any(item[1][1] > 0) and torch.all(item[1][2] == 0) and
+        torch.any(item[1][3] > 0) and torch.all(item[1][4] == 0) and torch.all(item[1][5] == 0) and
+        torch.any(item[1][6] > 0) and torch.any(item[1][7] > 0) and torch.any(item[1][8] > 0)
+    )
